9ffa0e8145f6c52aea4b4323e4453840fc19e9bc
MuhammadZainAmmad <zainammad05@gmail.com>
2021-01-23 15:02:26 +0500

    removed first testing open in web feature

--- app/src/main/java/com/example/cewtask2cs_1909619079/MainActivity.java
+++ app/src/main/java/com/example/cewtask2cs_1909619079/MainActivity.java
@@ -10,23 +10,11 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    EditText etOpenLink ;
-    Button btnOpenLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        etOpenLink = (EditText) findViewById(R.id.et_openWeb);
-        btnOpenLink = (Button) findViewById(R.id.btn_openWeb);
 
-        btnOpenLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String link = etOpenLink.getText().toString();
-                Uri uri = Uri.parse(link) ;
-                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
-            }
-        });
     }
 }
